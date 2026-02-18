06e683b3083d7c8aa1eb2fb6f95f697263bdb32b
richardmao77 <142338386+richardmao77@users.noreply.github.com>
2026-02-18 15:10:59 -0600

    Add files via upload

---
+++ Create Table.sql
@@ -0,0 +1,6 @@
+-- Run following SQL script to create a table in APEX PRD database
+-- Created by Haresh Jayakumar on Feb 18th 2026
+
+CREATE TABLE  "SPECIAL_SHIP_TEST" 
+   (  "STORE" NUMBER NOT NULL ENABLE
+   )
